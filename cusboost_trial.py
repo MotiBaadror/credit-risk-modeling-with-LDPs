9c3e4cb8a1b9c1f6583dc2cac110b9de2a2d4ee6
Moti Ram <motibaadror@gmail.com>
2021-05-15 18:59:35 +0530

    regression working

--- cusboost_trial.py
+++ cusboost_trial.py
@@ -1,15 +1,16 @@
 import numpy as np
-from sklearn.metrics import roc_curve, precision_recall_curve
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 from scipy import interp
 import pandas as pd
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostRegressor
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import Normalizer
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn.utils import shuffle
 import math
-from sklearn.metrics import roc_auc_score, average_precision_score, matthews_corrcoef, f1_score, accuracy_score, balanced_accuracy_score, confusion_matrix
+from sklearn.metrics import  average_precision_score, matthews_corrcoef, f1_score, accuracy_score, balanced_accuracy_score, confusion_matrix
 from sklearn.model_selection import StratifiedKFold
 import seaborn as sns
 
@@ -73,11 +74,13 @@ df['Credit amount'] = np.log(df['Credit amount'])
 # df = df.merge(pd.get_dummies(df.Risk, prefix='Risk'), left_index=True, right_index=True)
 # del df['Risk']
 # del df['Risk_good']
-df.to_csv('../data/features_data.csv', index=False)
+# df.to_csv('../data/features_data.csv', index=False)
 
 # Separate X and y of dataset
-X = np.array(df.drop(['Risk_bad'], axis=1))
-y = np.array(df['Risk_bad'])
+# np.random.shuffle(df)
+X = np.array(df.drop(['Credit amount'], axis=1))
+y = np.array(df['Credit amount'])
+shuffle(X,y)
 print("X:", X, '\n')
 # print("y:", y, '\n')
 
@@ -89,11 +92,11 @@ normalization_object = Normalizer()
 
 
 # K-fold cross validation that splits data into train and test set
-skf = StratifiedKFold(n_splits=5, shuffle=True) # default n_splits is 5
+# skf = StratifiedKFold(n_splits=5, shuffle=True) # default n_splits is 5
 
-# Record highest AUC and MCC
-top_auc = 0
-top_mcc = 0
+# Record highest mae and mse
+top_mae = 0
+top_mse = 0
 # top_f1 = 0
 # top_acc = 0
 # top_bal_acc = 0
@@ -109,15 +112,15 @@ percentage_to_choose_from_each_cluster = 0.5 # Undersampling ratio of 0.5 for ea
 for depth in range(2, 20, 10): # What is depth and estimators?
     for estimators in range(20, 50, 10):
 
-        current_param_auc = []
-        current_param_mcc = []
+        current_param_mae = []
+        current_param_mse = []
         # current_param_f1 = []
         # current_param_acc = []
         # current_param_bal_acc = []
         current_param_aupr = []
         tprs = []
 
-        for train_index, test_index in skf.split(X, y):
+        for train_index, test_index in [np.split(np.arange(len(df)),[int(len(df)*.80)])]:
             # print('train_index:', train_index)
             # print('test_index:', test_index)
 
@@ -132,52 +135,52 @@ for depth in range(2, 20, 10): # What is depth and estimators?
             # print('y_test:', y_test)
             
             # Cluster majority class instances
-            value, counts = np.unique(y_train, return_counts=True)
-            minority_class = value[np.argmin(counts)]
-            majority_class = value[np.argmax(counts)]
+            # value, counts = np.unique(y_train, return_counts=True)
+            # minority_class = value[np.argmin(counts)]
+            # majority_class = value[np.argmax(counts)]
 
-            idx_min = np.where(y_train == minority_class)[0]
-            idx_maj = np.where(y_train == majority_class)[0]
+            # idx_min = np.where(y_train == minority_class)[0]
+            # idx_maj = np.where(y_train == majority_class)[0]
 
-            majority_class_instances = X_train[idx_maj]
-            majority_class_labels = y_train[idx_maj]
+            # majority_class_instances = X_train[idx_maj]
+            # majority_class_labels = y_train[idx_maj]
 
-            kmeans = KMeans(n_clusters=number_of_clusters)
-            kmeans.fit(majority_class_instances)
+            # kmeans = KMeans(n_clusters=number_of_clusters)
+            # kmeans.fit(majority_class_instances)
 
-            X_maj = []
-            y_maj = []
+            # X_maj = []
+            # y_maj = []
 
-            points_under_each_cluster = {i: np.where(kmeans.labels_ == i)[0] for i in range(kmeans.n_clusters)}
+            # points_under_each_cluster = {i: np.where(kmeans.labels_ == i)[0] for i in range(kmeans.n_clusters)}
 
             
             # Choose majority class instances and add to dataset to use
-            for key in points_under_each_cluster.keys():
-                points_under_this_cluster = np.array(points_under_each_cluster[key])
-                number_of_points_to_choose_from_this_cluster = math.ceil(
-                    len(points_under_this_cluster) * percentage_to_choose_from_each_cluster)
-                selected_points = np.random.choice(points_under_this_cluster,
-                                                   size=number_of_points_to_choose_from_this_cluster)
-                X_maj.extend(majority_class_instances[selected_points])
-                y_maj.extend(majority_class_labels[selected_points])
+            # for key in points_under_each_cluster.keys():
+            #     points_under_this_cluster = np.array(points_under_each_cluster[key])
+            #     number_of_points_to_choose_from_this_cluster = math.ceil(
+            #         len(points_under_this_cluster) * percentage_to_choose_from_each_cluster)
+            #     selected_points = np.random.choice(points_under_this_cluster,
+            #                                        size=number_of_points_to_choose_from_this_cluster)
+            #     X_maj.extend(majority_class_instances[selected_points])
+            #     y_maj.extend(majority_class_labels[selected_points])
 
-            X_sampled = np.concatenate((X_train[idx_min], np.array(X_maj)))
-            y_sampled = np.concatenate((y_train[idx_min], np.array(y_maj)))
+            X_sampled = X_train # np.concatenate((X_train[idx_min], np.array(X_maj)))
+            y_sampled = y_train #np.concatenate((y_train[idx_min], np.array(y_maj)))
 
-            # Use AdaBoost as ensemble classifier of Decision Trees
-            classifier = AdaBoostClassifier(
-                DecisionTreeClassifier(max_depth=depth),
+            # Use AdaBoost as ensemble regressor of Decision Trees
+            regressor = AdaBoostRegressor(
+                DecisionTreeRegressor(max_depth=depth),
                 n_estimators=estimators,
-                learning_rate=1, algorithm='SAMME') # SAMME discrete boosting algorithm, SAMME.R real boosting algorithm (converges faster)
+                learning_rate=0.001) # SAMME discrete boosting algorithm, SAMME.R real boosting algorithm (converges faster)
             
             
-            # Train classifier
-            classifier.fit(X_sampled, y_sampled)
-            # print("Trained classifier :", classifier.fit(X_sampled, y_sampled))
+            # Train regressor
+            regressor.fit(X_sampled, y_sampled)
+            # print("Trained regressor :", regressor.fit(X_sampled, y_sampled))
             
             # Make predictions on test data
-            predictions = classifier.predict_proba(X_test) # Nx2 array of probabilities in class 0 (good) and class 1 (bad) where N is 1000/(# splits)
-            y_pred = classifier.predict(X_test) # Returns N predicted y and agrees with probabilities
+            predictions = regressor.predict(X_test) # Nx2 array of probabilities in class 0 (good) and class 1 (bad) where N is 1000/(# splits)
+            y_pred = regressor.predict(X_test) # Returns N predicted y and agrees with probabilities
             # y_test_all.extend(y_test)
             # y_pred_all.extend(y_pred)
             
@@ -186,38 +189,38 @@ for depth in range(2, 20, 10): # What is depth and estimators?
             # print("y_pred :", y_pred)
             
 
-            # Calculate AUC and MCC of current split with specified depth and estimators
-            auc = roc_auc_score(y_test, predictions[:, 1]) # predictions[:, 1] returns only second column (probability of bad)
-            mcc = matthews_corrcoef(y_test, y_pred)
+            # Calculate mae and mse of current split with specified depth and estimators
+            mae = mean_absolute_error(y_test, predictions) # predictions[:, 1] returns only second column (probability of bad)
+            mse = mean_squared_error(y_test, predictions)
             # f1 = f1_score(y_test, y_pred)
             # acc = accuracy_score(y_test, y_pred)
             # bal_acc = balanced_accuracy_score(y_test, y_pred)
-            aupr = average_precision_score(y_test, predictions[:, 1])
+            # aupr = average_precision_score(y_test, predictions[:, 1])
 
-            current_param_auc.append(auc)
-            current_param_mcc.append(mcc)
-            # current_param_mcc.append(f1)
-            # current_param_mcc.append(acc)
-            # current_param_mcc.append(bal_acc)
-            current_param_aupr.append(aupr)
+            current_param_mae.append(mae)
+            current_param_mse.append(mse)
+            # current_param_mse.append(f1)
+            # current_param_mse.append(acc)
+            # current_param_mse.append(bal_acc)
+            # current_param_aupr.append(aupr)
 
-            fpr, tpr, thresholds = roc_curve(y_test, predictions[:, 1])
-            tprs.append(interp(mean_fpr, fpr, tpr))
-            tprs[-1][0] = 0.0
+            # fpr, tpr, thresholds = roc_curve(y_test, predictions[:, 1])
+            # tprs.append(interp(mean_fpr, fpr, tpr))
+            # tprs[-1][0] = 0.0
 
-        current_mean_auc = np.mean(np.array(current_param_auc))
-        current_mean_mcc = np.mean(np.array(current_param_mcc))
+        current_mean_mae = np.mean(np.array(current_param_mae))
+        current_mean_mse = np.mean(np.array(current_param_mse))
         # current_mean_f1 = np.mean(np.array(current_param_f1))
         # current_mean_acc = np.mean(np.array(current_param_acc))
         # current_mean_bal_acc = np.mean(np.array(current_param_bal_acc))
-        current_mean_aupr = np.mean(np.array(current_param_aupr))
+        # current_mean_aupr = np.mean(np.array(current_param_aupr))
 
         
-        # Compare new AUC with current best
-        if top_auc < current_mean_auc:
-        # if top_mcc < current_mean_mcc:
-            top_auc = current_mean_auc
-            top_mcc = current_mean_mcc
+        # Compare new mae with current best
+        if top_mae < current_mean_mae:
+        # if top_mse < current_mean_mse:
+            top_mae = current_mean_mae
+            top_mse = current_mean_mse
             # top_f1 = current_mean_f1
             # top_acc = current_mean_acc
             # top_bal_acc = current_mean_bal_acc
@@ -225,21 +228,21 @@ for depth in range(2, 20, 10): # What is depth and estimators?
             best_depth = depth
             best_estimators = estimators
 
-            best_auc = top_auc
-            best_mcc = top_mcc
+            best_mae = top_mae
+            best_mse = top_mse
             # best_f1 = top_f1
             # best_acc = top_acc
             # best_bal_acc = top_bal_acc
-            best_aupr = current_mean_aupr
+            # best_aupr = current_mean_aupr
             
-            # print("top_auc :", top_auc)
-            # print("best_auc :", best_auc)
+            # print("top_mae :", top_mae)
+            # print("best_mae :", best_mae)
             
-            best_tpr = np.mean(tprs, axis=0)
-            best_fpr = mean_fpr
+            # best_tpr = np.mean(tprs, axis=0)
+            # best_fpr = mean_fpr
 
-            best_precision, best_recall, _ = precision_recall_curve(y_test, predictions[:, 1])
-            best_fpr, best_tpr, thresholds = roc_curve(y_test, predictions[:, 1])
+            # best_precision, best_recall, _ = precision_recall_curve(y_test, predictions[:, 1])
+            # best_fpr, best_tpr, thresholds = roc_curve(y_test, predictions[:, 1])
 
 
 print('plotting', dataset)
@@ -251,22 +254,22 @@ print("best_estimators :", best_estimators)
 
 
 # Precision-Recall and ROC curves
-plt.plot(best_recall, best_precision, lw=2, color='Blue',
-         label='Precision-Recall Curve')
-plt.plot(best_fpr, best_tpr, lw=2, color='red',
-         label='ROC curve')
+# plt.plot(best_recall, best_precision, lw=2, color='Blue',
+#          label='Precision-Recall Curve')
+# plt.plot(best_fpr, best_tpr, lw=2, color='red',
+#          label='ROC curve')
 
-plt.xlabel('Recall')
-plt.ylabel('Precision')
-plt.ylim([0.0, 1.05])
-plt.xlim([0.0, 1.0])
-plt.legend(loc="upper right")
-plt.show()
+# plt.xlabel('Recall')
+# plt.ylabel('Precision')
+# plt.ylim([0.0, 1.05])
+# plt.xlim([0.0, 1.0])
+# plt.legend(loc="upper right")
+# plt.show()
 
 
 # Evaluate model with other metrics
-print("AUC :", best_auc)                    # Higher is better
-print("MCC :", best_mcc)                    # Closer to 1 is better
+print("mae :", best_mae)                    # Higher is better
+print("mse :", best_mse)                    # Closer to 1 is better
 # print("F1 Score :", best_f1)                # Closer to 1 is better
 # print("Accuracy :", best_acc)               # Closer to 1 is better
 # print("Balanced Accuracy :", best_bal_acc)  # Best is 1, worst is 0
